b09aab6fe4898a02fac8d5bd7e9b35e2fb056c07
Subhashis Pradhan <pradhan.subhasis174@gmail.com>
2025-02-24 22:28:26 +0530

    fix: update formula

--- Formula/vault.rb
+++ Formula/vault.rb
@@ -1,7 +1,7 @@
 class CliPass < Formula
   desc "Command Line Utility for managing passwords"
-  homepage "https://github.com/fagom/homebrew-vault"
-  url "https://github.com/fagom/homebrew-vault/releases/download/v1.0.0/vault-mac"
+  homepage "https://github.com/fagom/vault-cli"
+  url "https://github.com/fagom/vault-cli/releases/download/v1.0.0/vault-mac"
   sha256 "<MAC_BINARY_SHA256>"
   version "1.0.0"
 
